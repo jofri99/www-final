2182ed171013ad237f4083a8a3629117bb0af8dc
jofri <joofriii@googlemail.com>
2020-05-30 20:54:35 +0200

    init commit

---
+++ precache-manifest.90caaaa9ca3eeeb3b608191890dcfb52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761164742f1d4db4c0423dab835d9ca6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "047f7479a66d33aa430c",
+    "url": "/static/js/2.80e7fcca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.80e7fcca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48fe0f0a165bf9fc4d2e",
+    "url": "/static/js/main.22733f43.chunk.js"
+  },
+  {
+    "revision": "ac788e328d6986e5bfd5",
+    "url": "/static/js/runtime-main.3038bde8.js"
+  }
+]);
